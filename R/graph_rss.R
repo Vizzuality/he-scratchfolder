6dda6933a9c3cf372f4d774ab612c8ef6a97c996
Greta <greta.carrete@vizzuality.com>
2021-03-19 20:54:01 +0100

    added profile graph

--- R/graph_rss.R
+++ R/graph_rss.R
@@ -34,3 +34,44 @@ ggplot(data = d %>%
   ggtitle("Testing spatial rapid summaries")+
   theme_bw()+
   theme(legend.position = "top")
+
+
+
+
+
+####profile
+library(reshape2)
+d = read.csv("/Users/gretacvega/Desktop/steps/Sheet 2-Table 1.csv")
+dd = d %>% 
+  add_row(Geoprocessing.Step = "Start", no_python=0, Python=0, 
+          .before = 1) %>%
+  mutate(id = as.numeric(rownames(.))) %>% 
+  mutate(noPythonCum = cumsum(no_python), pythonCum = cumsum(Python)) %>% 
+  mutate(noPythonCum = noPythonCum/sum(d$no_python), pythonCum = pythonCum/sum(d$Python)) %>%
+  melt(id.vars = c("id", "Geoprocessing.Step", 
+                   #"steptype", 
+                   "no_python", "Python" )) %>% 
+  mutate( steptype = ifelse((no_python == 0 & variable == "noPythonCum"),1,
+                             ifelse((Python == 0 & variable == "pythonCum"),
+                                    1,0)))
+
+ggplot(data = dd, aes(x = id, y = value))+
+  geom_line(aes( group = variable, colour = variable))+
+  geom_point(aes(alpha=as.factor(steptype), colour = variable))+
+  geom_text(data = dd %>% filter(steptype == 0 & Geoprocessing.Step != "Calculate Value"),
+            aes(label=Geoprocessing.Step, colour = variable,), check_overlap = TRUE, size = 3,
+            nudge_x = 0.25,
+            nudge_y = -0.02, 
+            hjust = 0)+
+  geom_text(data = dd %>% filter(steptype == 0 & Geoprocessing.Step == "Calculate Value"),
+            aes(label=Geoprocessing.Step, colour = variable), check_overlap = TRUE, size = 5, x =c(3,10.8),y =c(0.9,0.999),
+            nudge_y = 0.05, 
+            nudge_x = -2,
+            hjust = 0)+
+  scale_alpha_manual(values = c(1,0), guide = 'none')+
+  scale_y_continuous(name = "Processing time", labels = scales::percent)+
+  scale_x_continuous(name = "Geoprocessing Steps", breaks = seq(-2,14,2), limits = c(0,14))+
+  scale_colour_manual(name = "Uses Calculate Value", labels = c("FALSE", "TRUE"), values = c("red", "blue") )+
+  theme_bw()+
+  theme(legend.position = "top")+
+  ggtitle("Comparison of workflows in arcgis Pro")
