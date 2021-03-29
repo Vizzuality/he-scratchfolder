a0d925d9891e0f0090aeb56016aec3c51a6459a3
Greta <greta.carrete@vizzuality.com>
2021-03-29 19:09:09 +0200

    comparing two workflows

--- R/graph_rss.R
+++ R/graph_rss.R
@@ -1,32 +1,41 @@
 d = read.csv("/Users/gretacvega/rapid_test.csv")
 head(d)
 summary(d)
+d$Gp = ifelse(d$Gp == "RstZsatTable", "No Calculate Value", "Calculate Value")
 library(ggplot2)
 library(plyr)
 library(dplyr)
 
-m = lm(time_s ~ size_km2, d %>% 
-     filter(success==TRUE))
+give_lab= function(gp, df = d){
+  m = lm(time_s ~ size_km2, df %>% 
+           filter(success==TRUE & Gp == gp))
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(unname(coef(m)[1]), digits = 2),
+                        b = format(unname(coef(m)[2]), digits = 2),
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  lab = as.character(as.expression(eq))
+  return(lab)
+}
+lab_df = as.data.frame(rbind (c(lab = give_lab(gp = "No Calculate Value"), Gp = "No Calculate Value"), 
+                c(lab = give_lab(gp = "Calculate Value"), Gp = "Calculate Value")))
 
-eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                 list(a = format(unname(coef(m)[1]), digits = 2),
-                      b = format(unname(coef(m)[2]), digits = 2),
-                      r2 = format(summary(m)$r.squared, digits = 3)))
-lab = as.character(as.expression(eq))
+
+
 
 ggplot(data = d %>% 
          filter(success==TRUE), aes(x = size_km2, y = time_s))+
   geom_smooth(method='lm', formula= y~x, lty = 2, fill = "blue", alpha =0.2)+
-  geom_point(aes(  shape = is_coastal, fill = success, colour = success), 
+  geom_point(aes(  shape = simple_shape, fill = success, colour = success), 
              #fill = "blue", colour = "blue",
              size = 5, alpha = 0.6)+
   geom_point(data = d %>% 
-               filter(success==FALSE), aes(x = size_km2, y = time_s, shape = is_coastal, fill = success, colour = success),
+               filter(success==FALSE), aes(x = size_km2, y = time_s, shape = simple_shape, fill = success, colour = success),
              #fill = "red", colour = "red",
              size = 5, alpha = 0.6)+
   #geom_text(aes(x = size_km2, y = time_s, label = id), nudge_x = 100)+
-  geom_text(x = 500000, y = 75, label = lab, parse = TRUE)+
-  scale_shape_manual(values = c(22,21), name = "The polygon covers land and sea")+
+  geom_text(data = lab_df, aes(label = lab), x = 1000000, y = 75,  parse = TRUE)+
+  facet_grid(Gp ~. )+
+  scale_shape_manual(values = c(21,22), name = "The polygon is simple")+
   scale_fill_manual(values = c("red", "blue"), name = "Job succeeded")+
   scale_colour_manual(values = c("red", "blue"), name = "Job succeeded")+
   scale_x_continuous(breaks= seq(0,5000000, 500000), name = "Area in sqkm")+
